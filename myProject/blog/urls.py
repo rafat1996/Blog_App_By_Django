8ddc3a57691e1e73a1b9ab78eefd08cc5962ef30
Your Name <youremail@example.com>
2023-10-07 13:50:43 +0300

    Module 1

---
+++ myProject/blog/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path
+
+app_name='blog'
+
+urlpatterns=[
+    path('', views.post_list,name='post_list'),
+    path('<int:id>/',views.post_detail,name='post_detail')
+]
---
+++ myProject/blog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render,get_object_or_404
+from .models import Post
+from django.http import Http404
+
+def post_list(request):
+    posts=Post.objects.all()
+    return render(request,'blog/post/post-list.html',{'posts':posts})
+
+
+def post_detail(request,id):
+    post=get_object_or_404(Post,id=id,status=Post.Status.PUBLISHED)
+    return render(request,'blog/post/post-detail.html',{'post':post})
+
+
+
